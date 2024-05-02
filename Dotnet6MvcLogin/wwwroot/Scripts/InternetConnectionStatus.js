2974ed1e36a6906de05d0796427d51db3a1dd763
enoxkage <enoxkage@gmail.com>
2024-05-02 11:28:09 +0200

    Add project files.

---
+++ Dotnet6MvcLogin/wwwroot/Scripts/InternetConnectionStatus.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+
+    // Listen for online/offline events
+    $(window).on('online', function () {
+        online();
+    });
+
+    $(window).on('offline', function () {
+        offline();
+    });
+
+    //styling of response online or offline
+    function online() {
+        $('#Box').css('background-color', 'green');
+        $('span').text('Online');
+    }
+
+    function offline() {
+        $('#Box').css('background-color', 'red');
+        $('span').text('Offline');
+    }
+
+    //using ajax to send a GET request to an api for internet connection
+    $.ajax({
+        url: "https://jsonplaceholder.typicode.com/todos/1",
+        method: "GET",
+        success: function () {
+            //if connection to the internet is a success a response is given stating that you are online
+            online();
+        },
+        error: function () {
+            //if there is no connection to the internet a response is given stating that you are offline
+            offline();
+        }
+    });
+});
+
+
+
---
+++ Dotnet6MvcLogin/wwwroot/Scripts/conSpeed.js
@@ -0,0 +1,20 @@
+﻿window.onload = function () {
+
+    checkConnectionSpeed();
+}
+
+var downlink = navigator.connection.downlink;
+
+
+function checkConnectionSpeed() {
+    var x = document.getElementById('connection-message');
+
+
+    if (downlink > 1.0) {
+        x.style.backgroundColor = 'green';
+        x.innerHTML = 'Your internet connection looks good';
+    } else {
+        x.style.backgroundColor = 'red';
+        x.innerHTML = 'Your internet connection is too slow';
+    }
+}
